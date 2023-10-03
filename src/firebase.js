4c5b535fb6f43f72364d410bf88391d8ff6c05d2
1eehanjin <12181670@office.inha.ac.kr>
2023-10-03 10:18:06 +0900

    2장

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from "firebase/compat/app"
+
+const firebaseConfig = {
+    apiKey: "AIzaSyC2aUI5netz1kAZBiqV99avVcTSDb4sjnE",
+    authDomain: "nwitter-10548.firebaseapp.com",
+    projectId: "nwitter-10548",
+    storageBucket: "nwitter-10548.appspot.com",
+    messagingSenderId: "822116676168",
+    appId: "1:822116676168:web:2367dc97ccc0318651de7d"
+  };
+
+
+export default firebase.initializeApp(firebaseConfig)
